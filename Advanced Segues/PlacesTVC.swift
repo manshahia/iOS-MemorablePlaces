d3fc72046b5a48ff4d3a50a0e8b0f29fa30825c1
Ravi Inder Manshahia <ravinder@lab91.co>
2018-11-11 09:25:15 +0530

    Maps Geocoding Reverse Forward Segues prepare & Perform Userdefaults placemark annotation

---
+++ Advanced Segues/PlacesTVC.swift
@@ -0,0 +1,126 @@
+//
+//  PlacesTVC.swift
+//  Advanced Segues
+//
+//  Created by Ravi Inder Manshahia on 10/11/18.
+//  Copyright © 2018 Ravi Inder Manshahia. All rights reserved.
+//
+
+import UIKit
+
+class PlacesTVC: UITableViewController {
+
+    var citiesArray = [String]()
+    var selectedPlace = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let objArray = UserDefaults.standard.object(forKey: "city") as? [String]
+        {
+            citiesArray = objArray
+            tableView.reloadData()
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return citiesArray.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")//tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = citiesArray[indexPath.row]
+        // Configure the cell...
+
+        return cell
+    }
+ 
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! UITableViewCell
+        selectedPlace = (cell.textLabel?.text)!
+        performSegue(withIdentifier: "visitPlace", sender: self)
+    
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        if segue.identifier == "visitPlace"
+        {
+            var destinationVC = segue.destination as! ViewController
+            destinationVC.placeToVisit = selectedPlace
+            destinationVC.addPlace = false
+        }
+        
+        if segue.identifier == "addAPlace"
+        {
+            var destinationVC = segue.destination as! ViewController
+            
+            destinationVC.addPlace = true
+            print("Preparing for add place")
+
+        }
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+ 
+ 
+
+}
--- Advanced Segues/ViewController.swift
+++ Advanced Segues/ViewController.swift
@@ -7,14 +7,128 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    var placeToVisit = ""
+    var addPlace = false
+    var citiesArray = [String]()
 
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Place to Visit is" , placeToVisit)
+        
+        if addPlace
+        {
+            addLongPressGestureRecogniser()
+            if let objArray = UserDefaults.standard.object(forKey: "city") as? [String]
+            {
+                citiesArray = objArray
+            }
+            
+        }
+        else
+        {
+            forwardGeocodeAndUpdateMap()
+        }
+        
+        
+    }
+    
+    func addLongPressGestureRecogniser()
+    {
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(longPressAction(gestureRecogniser:)))
+        uilpgr.minimumPressDuration = 2.0
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    @objc func longPressAction(gestureRecogniser : UIGestureRecognizer)
+    {
+        let touchPoint = gestureRecogniser.location(in: self.map)
+        let location2d = map.convert(touchPoint, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = "New place"
+        annotation.subtitle = "I Will go here"
+        annotation.coordinate = location2d
+        
+        map.addAnnotation(annotation)
+        
+        print("Lat : \(location2d.latitude) and Long : \(location2d.longitude)")
+        
+        //reverse Geocode to
+        let location = CLLocation(latitude: location2d.latitude, longitude: location2d.longitude)
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if error == nil {
+                if let placemark = placemarks?[0]
+                {
+                    if let cityName = placemark.subAdministrativeArea
+                    {
+                        if self.citiesArray.contains(cityName)
+                        {
+                            print("City Already Exists in DB")
+                        }
+                        else
+                        {
+                            self.citiesArray.append(cityName)
+                            UserDefaults.standard.setValue(self.citiesArray, forKey: "city")
+                            print(placemark.subAdministrativeArea!)
+                        }
+                        
+
+                    }
+                    else
+                    {
+                        print("Sub ADministrative ARea not found")
+                    }
+                
+                    print("CXomplete placemark is ", placemark)
+                }
+            }
+            else {
+                print("Error in Reverse Geocoding is ",error!)
+            }
+        }
     }
 
+    func forwardGeocodeAndUpdateMap()
+    {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(placeToVisit) { (placemarks, error) in
+            if error == nil
+            {
+                if let placemark = placemarks?[0]
+                {
+                    
+                    let latDelta = 0.05
+                    let longDelta = 0.05
+                    
+                    let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+                    let region = MKCoordinateRegionMake((placemark.location?.coordinate)!, span)
+                    
+                    self.map.setRegion(region, animated: true)
+                    print("Placemark is ",placemark)
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.title = self.placeToVisit
+                    annotation.coordinate = (placemark.location?.coordinate)!
+                    
+                    self.map.addAnnotation(annotation)
+                }
+            }
+            else
+            {
+                print("Error ::", error)
+            }
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
